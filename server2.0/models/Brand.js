41a9d8f8bbc306c8b98aacd2ccb851c8daf3a6d9
Devashish Rathod <rathod.devashish09@gmail.com>
2026-06-14 02:36:14 +0530

    package updated and removed unnessesary packages

--- server2.0/models/Brand.js
+++ server2.0/models/Brand.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 const {
+  isValidEmail,
   isValidPhoneNumber,
   isValidateMerchantId,
 } = require("../validator/common");
@@ -42,7 +42,7 @@ const brandSchema = new mongoose.Schema(
       lowercase: true,
       trim: true,
       validate: {
-        validator: validator.isEmail,
+        validator: (email) => isValidEmail(email),
         message: (props) => `${props.value} is not a valid email address`,
       },
     },
